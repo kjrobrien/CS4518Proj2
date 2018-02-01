d0e0c83bf7c504900192aa44a0ca0d99b5a87885
Sam Huang <shuang2@users.noreply.github.com>
2018-01-31 21:16:28 -0500

    Added new files

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/GalleryViewActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class GalleryViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_view);
+    }
+
+}
